8ab7b3d41bc539d58dd9f3ac0f8d82f16a988d19
Manvi Gupta <88962337+manvigupta505@users.noreply.github.com>
2025-10-31 16:46:15 +0530

    Update giftRoutes.js

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const logger = require('../logger');
-const connectToDatabase = require('../models/db'); 
+const connectToData = require('../models/db'); 
 
 // Get all gifts
 router.get('/', async (req, res, next) => {
-    logger.info('/ called');
-    try {
-       
-    } catch (e) {
-        logger.console.error('oops something went wrong', e);
-        next(e);
-    }
-});
+ 
+    
+);
 
 // Get a single gift by ID
 
