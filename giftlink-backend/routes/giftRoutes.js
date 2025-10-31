07aee40603120014f96a37823ce3e0462484579b
Manvi Gupta <88962337+manvigupta505@users.noreply.github.com>
2025-10-31 16:49:11 +0530

    Update giftRoutes.js

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -12,8 +12,18 @@ router.get('/', async (req, res, next) => {
 // Get a single gift by ID
 
 
-
 // Add a new gift
+router.post('/', async (req, res, next) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const gift = await collection.insertOne(req.body);
+
+        res.status(201).json(gift.ops[0]);
+    } catch (e) {
+        next(e);
+    }
+});
 
 
 module.exports = router;
